5798aa173d062e1b23915ee4a2b2ad769edfff13
Kerem YEREBAKAN <keremeyrbkn@gmail.com>
2025-06-26 17:23:51 +0300

    Add project files.

---
+++ noktacikarici/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace noktacikarici
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            btnResimSec = new Button();
+            btnNoktaOlustur = new Button();
+            btnDisaAktar = new Button();
+            txtDosyaYolu = new TextBox();
+            label1 = new Label();
+            btnCizimiGoster = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(399, 26);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(389, 366);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // btnResimSec
+            // 
+            btnResimSec.Location = new Point(49, 60);
+            btnResimSec.Name = "btnResimSec";
+            btnResimSec.Size = new Size(118, 33);
+            btnResimSec.TabIndex = 1;
+            btnResimSec.Text = "Resim Seç";
+            btnResimSec.UseVisualStyleBackColor = true;
+            btnResimSec.Click += btnResimSec_Click;
+            // 
+            // btnNoktaOlustur
+            // 
+            btnNoktaOlustur.Location = new Point(49, 112);
+            btnNoktaOlustur.Name = "btnNoktaOlustur";
+            btnNoktaOlustur.Size = new Size(118, 33);
+            btnNoktaOlustur.TabIndex = 2;
+            btnNoktaOlustur.Text = "Nokta Oluştur";
+            btnNoktaOlustur.UseVisualStyleBackColor = true;
+            btnNoktaOlustur.Click += btnNoktaOlustur_Click;
+            // 
+            // btnDisaAktar
+            // 
+            btnDisaAktar.Location = new Point(49, 217);
+            btnDisaAktar.Name = "btnDisaAktar";
+            btnDisaAktar.Size = new Size(118, 33);
+            btnDisaAktar.TabIndex = 3;
+            btnDisaAktar.Text = "Dışa Aktar";
+            btnDisaAktar.UseVisualStyleBackColor = true;
+            btnDisaAktar.Click += btnDisaAktar_Click;
+            // 
+            // txtDosyaYolu
+            // 
+            txtDosyaYolu.Location = new Point(486, 411);
+            txtDosyaYolu.Name = "txtDosyaYolu";
+            txtDosyaYolu.Size = new Size(302, 23);
+            txtDosyaYolu.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(399, 415);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Dosya Yolu :";
+            // 
+            // btnCizimiGoster
+            // 
+            btnCizimiGoster.Location = new Point(49, 165);
+            btnCizimiGoster.Name = "btnCizimiGoster";
+            btnCizimiGoster.Size = new Size(118, 33);
+            btnCizimiGoster.TabIndex = 7;
+            btnCizimiGoster.Text = "Çizimi Göster";
+            btnCizimiGoster.UseVisualStyleBackColor = true;
+            btnCizimiGoster.Click += btnCizimiGoster_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnCizimiGoster);
+            Controls.Add(label1);
+            Controls.Add(txtDosyaYolu);
+            Controls.Add(btnDisaAktar);
+            Controls.Add(btnNoktaOlustur);
+            Controls.Add(btnResimSec);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button btnResimSec;
+        private Button btnNoktaOlustur;
+        private Button btnDisaAktar;
+        private TextBox txtDosyaYolu;
+        private Label label1;
+        private Button btnCizimiGoster;
+    }
+}
---
+++ noktacikarici/Form1.cs
@@ -0,0 +1,195 @@
+﻿using Emgu.CV.Structure;
+using Emgu.CV;
+
+namespace noktacikarici
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap orijinalResim;
+        private Bitmap cizilmisBitmap;
+        private string secilenDosyaYolu;
+        Image<Gray, byte> edges;
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void btnResimSec_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Bir resim seçin";
+            ofd.Filter = "Resim Dosyaları|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                // Resmi yükle
+                orijinalResim = new Bitmap(ofd.FileName);
+
+                // PictureBox'a resmi ata
+                pictureBox1.Image = orijinalResim;
+
+                // PictureBox sığdırma ayarı (bir kez Form_Load'da da verebilirsin)
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+                // Dosya yolunu göster
+                txtDosyaYolu.Text = ofd.FileName;
+
+                secilenDosyaYolu = ofd.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtDosyaYolu.Enabled = false;
+        }
+
+        private void btnNoktaOlustur_Click(object sender, EventArgs e)
+        {
+            if (orijinalResim == null)
+            {
+                MessageBox.Show("Lütfen önce bir resim seçin.");
+                return;
+            }
+
+            // ✅ Direkt dosya yolundan oku (Hatasız!)
+            Image<Bgr, byte> imageCV = new Image<Bgr, byte>(secilenDosyaYolu);
+
+            // 2. Griye çevir ve kenarları bul
+            Image<Gray, byte> gray = imageCV.Convert<Gray, byte>();
+            Image<Gray, byte> edges = gray.Canny(100, 200);
+
+            // 3. Kenar piksellerini al
+            List<Point> edgePoints = new List<Point>();
+            for (int y = 0; y < edges.Height; y++)
+            {
+                for (int x = 0; x < edges.Width; x++)
+                {
+                    if (edges.Data[y, x, 0] > 0)
+                        edgePoints.Add(new Point(x, y));
+                }
+            }
+
+            // 4. Seyrekleştirme (örnek: her 10. noktayı al)
+            //List<Point> sampledPoints = new List<Point>();
+            //for (int i = 0; i < edgePoints.Count; i += 40)
+            //    sampledPoints.Add(edgePoints[i]);
+            List<Point> sampledPoints = new List<Point>();
+            int hedefNoktaSayisi = 1000;
+
+            if (edgePoints.Count < hedefNoktaSayisi)
+            {
+                sampledPoints = edgePoints;
+            }
+            else
+            {
+                double adim = (double)edgePoints.Count / hedefNoktaSayisi;
+
+                for (int i = 0; i < hedefNoktaSayisi; i++)
+                {
+                    int index = (int)(i * adim);
+                    sampledPoints.Add(edgePoints[index]);
+                }
+            }
+
+
+            // 5. Yeni Bitmap'e noktaları ve numaraları çiz
+            //Bitmap sonuc = new Bitmap(edges.Width, edges.Height);
+            //using (Graphics g = Graphics.FromImage(sonuc))
+            //{
+            //    g.Clear(Color.White);
+            //    Font font = new Font("Arial", 14);
+            //    Brush brush = Brushes.Black;
+            //    int index = 1;
+            //    foreach (Point p in sampledPoints)
+            //    {
+            //        g.FillEllipse(brush, p.X - 2, p.Y - 2, 4, 4); // nokta
+            //        g.DrawString(index.ToString(), font, brush, p.X + 4, p.Y + 4); // numara
+            //        index++;
+            //    }
+            //}
+            //int a4Genislik = 2480;
+            //int a4Yukseklik = 3508;
+            //Bitmap sonuc = new Bitmap(a4Genislik, a4Yukseklik);
+
+            //using (Graphics g = Graphics.FromImage(sonuc))
+            //{
+            //    g.Clear(Color.White);
+            //    Font font = new Font("Arial", 6);
+            //    float oranX = (float)a4Genislik / edges.Width;
+            //    float oranY = (float)a4Yukseklik / edges.Height;
+
+            //    int index = 1;
+            //    foreach (Point p in sampledPoints)
+            //    {
+            //        float x = p.X * oranX;
+            //        float y = p.Y * oranY;
+
+            //        g.FillEllipse(Brushes.Black, x - 2, y - 2, 4, 4);
+            //        g.DrawString(index.ToString(), font, Brushes.Black, x + 4, y + 4);
+            //        index++;
+            //    }
+            //}
+
+            int a3Genislik = 3508;
+            int a3Yukseklik = 4961;
+            Bitmap sonuc = new Bitmap(a3Genislik, a3Yukseklik);
+
+            using (Graphics g = Graphics.FromImage(sonuc))
+            {
+                g.Clear(Color.White);
+                Font font = new Font("Arial", 12, FontStyle.Bold);
+                float oranX = (float)a3Genislik / edges.Width;
+                float oranY = (float)a3Yukseklik / edges.Height;
+
+                int index = 1;
+                foreach (Point p in sampledPoints)
+                {
+                    float x = p.X * oranX;
+                    float y = p.Y * oranY;
+
+                    g.FillEllipse(Brushes.Black, x - 6, y - 6, 12, 12);
+                    g.DrawString(index.ToString(), font, Brushes.Black, x + 10, y + 10);
+                    index++;
+                }
+            }
+
+            // 6. PictureBox'ta göster
+            pictureBox1.Image = sonuc;
+
+            // 7. Dışa aktar için kaydet
+            cizilmisBitmap = sonuc;
+        }
+
+        private void btnDisaAktar_Click(object sender, EventArgs e)
+        {
+            if (cizilmisBitmap == null)
+            {
+                MessageBox.Show("Önce bir çizim oluşturun.");
+                return;
+            }
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Çizimi Kaydet";
+            sfd.Filter = "PNG Dosyası|*.png";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    cizilmisBitmap.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                    MessageBox.Show("Çizim başarıyla kaydedildi!", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hata: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnCizimiGoster_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bu Özellik Geliştirilecek");
+        }
+    }
+}
